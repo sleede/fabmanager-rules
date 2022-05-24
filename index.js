adb8a5eba7685d5f3e5d855e597cbc4806189116
Sylvain <sylvain@sleede.com>
2022-05-24 11:30:23 +0200

    (bug) fix classname detection

--- index.js
+++ index.js
@@ -11,11 +11,30 @@ module.exports = {
         }
       },
       "create": (context) => {
-        return {
+        function findDeclaration (node) {
+          if (node.type === "VariableDeclarator") return node;
+          else return findDeclaration(node.parent);
+        }
+
+        function evaluateClassName(value) {
+          if (value.type === "Literal") {
+            return value.value;
+          }
+          if (value.type === "JSXExpressionContainer") {
+            return value.expression.quasis.map(q => q.value.raw).join();
+          }
+        }
+
+       return {
           JSXOpeningElement: (node) => {
-            const componentName = node.name.name;
-            const className = node.attributes.find((attr) => attr.name.name === "className");
-            if (className && className.value.value !== componentName) {
+            const componentName = findDeclaration(node).id.name;
+            const dashedName = componentName[0].toLowerCase() + componentName.substring(1).replace(/([A-Z])/g, val => `-${val.toLowerCase()}`);;
+
+            const classNameAttr = node.attributes.find((attr) => attr.name.name === "className");
+            const classes = evaluateClassName(classNameAttr.value);
+            const regex = new RegExp("(^|\\s)" + dashedName + "(\\s|$)");
+
+            if (classNameAttr && !classes.match(regex)) {
               context.report({
                 node,
                 messageId: "no-class-name",
