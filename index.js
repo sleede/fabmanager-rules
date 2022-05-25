8bc736497992f7014eff1787f5c2883b18ca7257
Sylvain <sylvain@sleede.com>
2022-05-25 15:54:26 +0200

    fix spacing

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = {
                 return className.value.expression.quasis.map(q => q.value.raw).join();
               }
             }
-	        }
+          }
           return '';
         }
 
